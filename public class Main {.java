abf06e16215af4248c1550f1313476b9396fc055
y123sh <114246775+y123sh@users.noreply.github.com>
2023-03-09 23:47:51 +0800

    Add files via upload
    java猜数字，打印随机数

---
+++ public class Main {.java
@@ -0,0 +1,25 @@
+public class Class {
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		Scanner sc = new Scanner(System.in);//鍒涘缓瀵硅薄
+		int n;
+		boolean input;
+		do {
+			int num = rand.nextInt(45);
+			System.out.println(num);// 鐢熸垚闅忔満鏁�
+			Menu();
+			System.out.println("请选择:");
+			n = sc.nextInt();//
+			System.out.println(n);
+		} while (n == 1);
+	}
+	public static void Menu() {
+		System.out.println("*******************************");
+		System.out.println("****1.继续抽查 0.停止抽查 ********");
+		System.out.println("*******************************");
+		
+	}
+}
+
